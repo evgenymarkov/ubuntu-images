a5216a43fa4ff5d3fb684f6a49a5102c40892978
Evgeny Markov <evgenymarkov@yandex.ru>
2020-12-16 22:56:31 +0300

    feat: simplify bash prompt

--- library/prompt.sh
+++ library/prompt.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env sh
 set -e
 
-echo '' >> /home/ubuntu/.bashrc
-echo 'TERM=xterm-256color' >> /home/ubuntu/.bashrc
+{
+  echo ''
+  echo 'PS1="\W # "'
+  echo 'TERM="xterm-256color"'
+} >> /root/.bashrc
+
+{
+  echo ''
+  echo 'PS1="\W $ "'
+  echo 'TERM="xterm-256color"'
+} >> /home/ubuntu/.bashrc
